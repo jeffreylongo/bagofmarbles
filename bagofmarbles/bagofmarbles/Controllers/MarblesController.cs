8937c1751a0d2f642452f7eca18f173768208863
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-25 15:50:51 -0400

    apis work on swagger, but not onclick.

---
+++ bagofmarbles/bagofmarbles/Controllers/MarblesController.cs
@@ -0,0 +1,27 @@
+﻿using bagofmarbles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace bagofmarbles.Controllers
+{
+    public class MarblesController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Hello = "world" });
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddMarble(Marble marble)
+        {
+            marble.Id = new Random().Next(0, 100);
+            marble.Color = "red";
+            return Ok(marble);
+        }
+    }
+}
--- bagofmarbles/bagofmarbles/Scripts/custom.js
+++ bagofmarbles/bagofmarbles/Scripts/custom.js
@@ -1,4 +1,28 @@
 ﻿let doSomething = () => {
-    let _fullName = $("#name").val();
+    let _fullName = $("name").val();
     $("#welcomeMessage").html(_fullName);
+}
+
+let talkToServer = () => {
+
+    let newMarble = {
+        name: $("#newMarble").val()
+    };
+
+    $.ajax({
+        url: '/api/marbles',
+        dataType: "json",
+        contentType: "application/jason",
+        data: JSON.stringify(newMarble),
+        type: "POST",
+        success: (data) => {
+            $("#marbleList").append($("<li>").html(data.Name));
+        },
+        error: (data) => {
+            console.log("oops", data)
+        },
+        complete: (data) => {
+            console.log("done", data);
+        }
+    });
 }
